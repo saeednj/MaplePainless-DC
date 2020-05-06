eeea270e6fba5aa1f5ccde7219501b9f867dcda5
Saeed Nejati <nejati@gmail.com>
2020-05-06 12:19:17 -0400

    reading from aws path

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+if [[ "${COMP_S3_PROBLEM_PATH}" == *".xz" ]];
+then
+    aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} supervised-scripts/task.cnf.xz
+    unxz supervised-scripts/task.cnf.xz
+else
+    aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} supervised-scripts/task.cnf
+fi
 
-./painless -c=62 -solver=maple -wkr-strat=4 -split-heur=2 -copy-mode=2 -shr-strat=1 -lbd-limit=4  -init-act=1 -init-pol=1 -gaussian $1
+painless/painless -c=63 -solver=maple -wkr-strat=4 -split-heur=2 -copy-mode=2 -shr-strat=1 -lbd-limit=4  -init-act=1 -init-pol=1 -gaussian supervised-scripts/task.cnf
